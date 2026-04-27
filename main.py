50fc536bb3c27f172021bab30cae2fde98d9d315
Margaret Barnes <mzb6631@psu.edu>
2026-04-27 15:54:16 -0400

    updated so it has levels, need to add number of tries and make it more creative

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import random
 
-def number_generator():
+def number_generator(level):
     #returns a ranom number between 1 and 100
-    return random.randint(1, 100)
+    high = 0
+    if level=="easy":
+        high = 20
+    elif level=="medium":
+        high = 50
+    elif level=="hard":
+        high = 100
+    return random.randint(1, high)
 
 def checker(number, guess):
     #tests if a guess is =, >, or < than the number
@@ -14,14 +21,21 @@ def checker(number, guess):
     else:
         print("Too low")
         return False
-    
-number = number_generator()
-is_correct = False
 
-while is_correct == False:
-    #goes until the guess is = to the number, might add a guess limit later
-    curr_guess = int(input("Enter your guess!"))
-    is_correct = checker(number, curr_guess)
-print("Correct!")
+
+is_correct = False
+print("Welcome to the Nittany Number Game!")
+print("You can choose your own path")
+level = input("Enter your choice of difficulty: Easy, Medium, or Hard").lower()
+
+if level == "easy" or level == "medium" or level == "hard":
+    number = number_generator(level)
+    while is_correct == False:
+        #goes until the guess is = to the number, might add a guess limit later
+        curr_guess = int(input("Enter your guess!"))
+        is_correct = checker(number, curr_guess)
+    print("Correct!")
+else:
+    print("That's not a difficulty, restart")
 
     
